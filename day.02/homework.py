3f628afa6fb125a377652ca8e87dea3ce1ae5b18
nanuka614 <dekanosidze.nanuka@icloud.com>
2024-10-12 19:46:07 +0400

    second commit

---
+++ day.02/homework.py
@@ -0,0 +1,21 @@
+print("my name is nanuka")
+print("my surname is dekanosidze")
+print("i am 14 years old")
+print("i want to learn programming")
+print("GOA is the best")
+print("ცვლადი არის მონაცემთა შენახვის საშვალება")
+print("nanuka")
+print("dekanosidze")
+print(14)
+print("ცვლადის მეშვეობით ეკრანზე იბეჭდება ინფორმაცია" )
+print("ცვლადის სახელი=ცვლადის მნიშვნელობა")
+print("programming is very interesting")
+name="nanuka"
+surname="dekanosidze"
+age=14
+print(name)
+print(surname)
+print(age)
+
+
+
