14807cecc85ce321f918acb3ea584eb8c2efe8d7
nanuka614 <dekanosidze.nanuka@icloud.com>
2024-11-02 18:31:08 +0400

    second commit

---
+++ day.08/classwork/classwork.py
@@ -0,0 +1,21 @@
+name=input("enter your name: ")
+surname=input("enter your surname: ")
+age=input("enter your age: ")
+num1=input("enter your number: ")
+num2=input("enter your number: ")
+print(name)
+print(surname)
+print(age)
+print(num1)
+print(num2)
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(num1))
+print(type(num2))
+
+num1=input("enter your first number: ")
+num2=input("enter your second number: ")
+
+print(num1 + num2)
---
+++ day.08/homework/homework(2).py
@@ -0,0 +1,29 @@
+#ცვლადები
+
+num1=10
+num2=3
+num3=100
+num4=5
+num5=1
+
+#მათემატიკური ოპერაციები
+
+print(num4 % num1 + (num3 * num2) / num5)
+
+print((num1 + num2) / num3 - num4 * num5 + num1 ** num3 % num2)
+
+print((num1 * num2) + num3 - (num4 ** num5))
+
+print((num1 + num2) / num3 * num4)
+
+print(num4 * (num5 - num1) / (num3 ** 2) + (num1 * num2) % num4) 
+
+print(num4 / num2 * (num1 - num3 ** num5) + num1 % num3) 
+
+print((num1 % num4) + (num5 ** num3) / (num2 + num1))
+
+print((num2 * num4) - (num1 / num3) + (num5 ** 2) % num1)
+
+print(num1 ** (num3 + num2) % (num4 * num5) + (num2 - num1) ** 2)
+
+print((num1 + num2) ** num3 - (num4 / num5) * (num1 - num2))
