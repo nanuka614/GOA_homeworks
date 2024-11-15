67980d0169bc9aef90b125e57e3f1e8ad9efbcb0
nanuka614 <dekanosidze.nanuka@icloud.com>
2024-11-15 21:23:24 +0400

    second commit

---
+++ day.12/classwork/classwork.py
@@ -0,0 +1,14 @@
+print(True or True) #True
+print(True or False) #True
+print(False or True) #True
+print(False or False) #False
+
+print(True and True) #True
+print(True and False) #False
+print(False and True) #False
+print(False and False) #False
+
+
+num1=int(input("enter your number: "))
+num2=int(input("enter your number: "))
+print(num1 > 30 and num2 < 40)
---
+++ day.12/homework/homework.py
@@ -0,0 +1,30 @@
+print(True and False)  
+print(True and True)
+
+print(True or False)    
+print(False or False)   
+
+print(True and False or True)    
+print(False or True and False)
+
+a = 10
+b = 20
+c = 40
+d = 30
+e = 50
+
+print(a < b and b < c)             
+print(a > c or d < b)               
+print(a + d == c and c > e)       
+print(e - b > c or a < d)           
+print((a + d) == 30 and (e - b) == 20)
+print((e / d) == 6 or (a % d) == 1)
+print((a + b) > (c + d) and e > a)
+
+x = a > b                      
+y = c < e                
+
+print(x or y)                 
+print(x and y)                   
+print(not x)
+
