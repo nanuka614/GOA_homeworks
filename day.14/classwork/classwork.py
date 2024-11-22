99f52543e644789db8045cee7494ef0df77c2a69
nanuka614 <dekanosidze.nanuka@icloud.com>
2024-11-22 05:08:48 +0400

    second commit

---
+++ day.14/classwork/classwork.py
@@ -0,0 +1,18 @@
+for i in range(20):
+    print("ნანუკა დეკანოსიძე")
+
+
+num1, num2=int(input("შეიყვანეთ პირველი რიცხვი: ")),  int(input("შეიყვანეთ მეორე რიცხვი: "))
+for i in range(num1, num2):  
+    print(i**2)
+
+
+num1, num2, num3=3, 6, 9
+print(num1)
+print(num2)
+print(num3)
+
+
+n = int(input("შეიყვანეთ რიცხვი n: "))
+for i in range(1,n):
+    print(i ** 2)
---
+++ day.14/homework/homework.py
@@ -0,0 +1,35 @@
+range1 = range(20, 51)
+range2 = range(100, 151)
+range3 = range(20, 51, 2)
+range4=range(11, 32, 2)
+
+for i in range(10):
+    print("nanuka dekanosidze") 
+
+for i in range(10, 31):
+    print(i / 2)
+
+for i in range(40, 61):
+    print(i ** 3)
+
+for i in range(5):
+    num = int(input("enter your number: "))
+    print(num)
+
+name = input("enter your name: ")
+for letter in name:
+    print(letter)
+
+num1 = int(input("enter your first number: "))
+num2 = int(input("enter your second number: "))
+num3 = int(input("enter your third number: "))
+
+range= range(num1, num2, num3)
+for i in range:
+    print( i ** 2)
+
+sum = 0
+for num in range(5, 16):
+    sum += num 
+
+print(sum)
