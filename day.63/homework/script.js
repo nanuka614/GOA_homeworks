c27af9a3bcaacd974e8937df772b5d5bc77c9052
nanuka614 <dekanosidze.nanuka@icloud.com>
2025-05-18 15:37:00 +0400

    GOA BEST

---
+++ day.63/homework/script.js
@@ -0,0 +1,22 @@
+function showAlert() {
+    alert("Hello, world!");
+}
+
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+
+function multiplyByTen(number) {
+    return number * 10;
+}
+
+
+function changeParagraphText() {
+    document.getElementById("myParagraph").textContent = "The text has been changed!";
+}
